5ee323861e03acda6d162f6814e472b8845f2d50
sidhant kumar <sidhant2542021@gmail.com>
2026-09-23 23:51:55 +0530

    add Curd topic

---
+++ fsCurd.js
@@ -0,0 +1,42 @@
+const fs =require('fs');
+
+
+const operation =process.argv[2];
+if(operation=='write'){
+  const name =process.argv[3];
+  const content =process.argv[4];
+  const fullName ="files/"+name+".txt"
+  fs.writeFileSync(fullName,content)
+
+  //console.log(operation,name,content);
+}else if (operation =='read'){
+  const name = process.argv[3];
+  const fullName ='files/'+name+ ".txt"
+  let data =fs.readFileSync(fullName,"utf-8");
+  console.log(data);
+}
+else if (operation =='update'){
+  const name = process.argv[3];
+   const content =process.argv[4];
+  const fullName ='files/'+name+ ".txt"
+  let data =fs.appendFileSync(fullName,content);
+  console.log(data);
+}
+else if (operation =='delete'){
+  const name = process.argv[3];
+   //const content =process.argv[4];
+  const fullName ='files/'+name+ ".txt"
+  let data =fs.unlinkSync(fullName);
+  console.log(data);
+}else {
+  console.log("this operation is not folnd ");
+}
+
+
+//fs.writeFileSync("files/apple.txt","This is a fruit");
+//fs.unlinkSync("files/banana.txt");
+
+//const data =fs.readFileSync("files/apple.txt", 'utf-8');
+//console.log(data);
+
+//fs.appendFileSync("files/apple.txt"," and this is a good for health");
